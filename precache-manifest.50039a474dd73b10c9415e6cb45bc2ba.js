b62f05b62890e79dc0314358975ff805c90a8462
Nimish Lal <nlal@frontporch.com>
2021-08-09 13:13:54 -0700

    Updates

---
+++ precache-manifest.50039a474dd73b10c9415e6cb45bc2ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47d4a2add7fb14b4778ccdade30ff2ff",
+    "url": "/fiji-radio-batkau/index.html"
+  },
+  {
+    "revision": "6417d9af918010f9dd7f",
+    "url": "/fiji-radio-batkau/static/css/2.a3423c24.chunk.css"
+  },
+  {
+    "revision": "d6fa76b219acfc19671f",
+    "url": "/fiji-radio-batkau/static/css/main.4902ddcf.chunk.css"
+  },
+  {
+    "revision": "6417d9af918010f9dd7f",
+    "url": "/fiji-radio-batkau/static/js/2.abeabd29.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/fiji-radio-batkau/static/js/2.abeabd29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6fa76b219acfc19671f",
+    "url": "/fiji-radio-batkau/static/js/main.eb81089c.chunk.js"
+  },
+  {
+    "revision": "c9fdcca83b0829c64198",
+    "url": "/fiji-radio-batkau/static/js/runtime-main.4001c5ab.js"
+  },
+  {
+    "revision": "841f890a20ab5837d271910e2d5dd7ff",
+    "url": "/fiji-radio-batkau/static/media/pain.841f890a.jpg"
+  }
+]);
