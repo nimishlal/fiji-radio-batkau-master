c366a66976667460d68435c154a4f3f37c831834
Nimish Lal <nlal@frontporch.com>
2021-08-09 11:58:54 -0700

    Switch to yarn

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import test from './assets/test.jpeg'
 
 function App() {
   function refreshPage() {
@@ -67,6 +68,19 @@ function App() {
           Click to reload!
         </button>
       </div>
+
+      <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+  <div class="md:flex">
+    <div class="md:flex-shrink-0">
+      <img class="h-48 w-full object-cover md:h-full md:w-48" src={test} alt="Man looking at item at a store"></img>
+    </div>
+    <div class="p-8">
+      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
+      <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding customers for your new business</a>
+      <p class="mt-2 text-gray-500">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
+    </div>
+  </div>
+</div>
 
       <footer className="colorFooter text-center">
         Made By Chidoro Batkau V1.0 MixKarDo
